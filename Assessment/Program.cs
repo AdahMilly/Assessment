9e3c14da8078cb088c6e1d251ed2075d257639f6
AdahMilly <oamildred@gmail.com>
2023-11-30 16:51:01 +0300

    reverse integer

--- Assessment/Program.cs
+++ Assessment/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Assessment.MostAppearingCharacter;
 using Assessment.RemoveWhitespaces;
+using Assessment.ReverseIntegers;
 
 Console.WriteLine("Hello, World!");
 
@@ -10,3 +11,6 @@ mrc.mostAppearingCharacter("11211234");
 
 RemoveWhitespaces rmw = new RemoveWhitespaces();
 //rmw.removeWhitespace([1,0,3,4,5]);
+
+ReverseOrdering ordering = new ReverseOrdering();
+Console.WriteLine(ordering.reverseInt(1234));
--- Assessment/ReverseIntegers/ReverseOrdering.cs
+++ Assessment/ReverseIntegers/ReverseOrdering.cs
@@ -3,9 +3,23 @@ namespace Assessment.ReverseIntegers
 {
     internal class ReverseOrdering
     {
-        public void reverseInt(int[] input)
+        public int reverseInt(int input)
         {
+            string numberConversion = input.ToString(); //convert to string
+            List<string> numbers = new List<string>();
+           foreach(var letter in numberConversion)
+            {
+                numbers.Add(letter.ToString());
+            }// convert to character array
 
+            numbers.Reverse();
+            string concatenatedString="";
+            foreach(var number in numbers)
+            {
+                concatenatedString =concatenatedString+number;
+            }
+
+           return Convert.ToInt32(concatenatedString);
         }
     }
 }
