6c51ecc9f2bd1d6f9ab7b5eba4eb554451e53dd1
AdahMilly <oamildred@gmail.com>
2023-12-04 16:03:34 +0300

    anagram

--- Assessment/Anagrams/Anagram.cs
+++ Assessment/Anagrams/Anagram.cs
@@ -4,5 +4,12 @@ namespace Assessment.Anagrams
 {
     internal class Anagram
     {
+        public bool IsAnagram(string str1, string str2) {
+            //string cleanedStr1 = new string(str1.ToLower().Where(c => !char.IsWhiteSpace(c)).ToArray());
+            //string cleanedStr2 = new string(str2.ToLower().Where(c => !char.IsWhiteSpace(c)).ToArray());
+
+            // Check if the sorted characters of both strings are the same
+            //return string.Concat(cleanedStr1.OrderBy(c => c)) == string.Concat(cleanedStr2.OrderBy(c => c));
+        }
     }
 }
